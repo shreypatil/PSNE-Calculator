cf976d40a0b47382307adf4420a4907e75e13e43
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2022-03-19 03:40:15 +0530

    Fix Input Format (array creation)

--- inout.py
+++ inout.py
@@ -14,11 +14,17 @@ def get_data_from(file_path) :
     assert len(strat_count) == player_count, "Number of strategies must be specified for each Player"
     
     element_count = player_count
-    dim = [player_count]
-    for i in strat_count :
-        dim.append(int(i))
-        element_count *= int(i)
+    dim = []
+    transpose_axes = [player_count]
+
+    for idx, val in enumerate(strat_count) :
+        dim.append(int(val))
+        transpose_axes.append(idx)
+        element_count *= int(val)
     
+    dim.append(player_count)
+    transpose_axes.reverse()
+
     elements = lines[2].split(' ')
     assert len(elements) == element_count, " Incorrect number of Utilities Provided"
     
@@ -27,8 +33,7 @@ def get_data_from(file_path) :
     for idx, val in enumerate(elements) :
         strat_matrix[idx] = float(val)
     
-    
-    return strat_matrix.reshape(tuple(dim))
-        
+    reshaped = strat_matrix.reshape(tuple(dim))
+    return reshaped.transpose(transpose_axes)
     
     f.close()
