51b026ded314a09aa0d4c8f648eb9eb3e7c9491b
Shikhar Saxena <56793913+shikhar-saxena@users.noreply.github.com>
2022-03-19 15:06:06 +0530

    Fix Input Format
    Remove input.py and move input function in psne.py

--- psne.py
+++ psne.py
@@ -1,14 +1,39 @@
-import numpy
-import sys
+import numpy as np
 
-from inout import get_data_from
+def get_input():
+    
+    player_count = int(input().strip())
+    
+    strat_count = input().strip().split(' ')
+    assert len(strat_count) == player_count, "Number of strategies must be specified for each Player"
+    
+    element_count = player_count
+    dim = [player_count]
+    transpose_axes = [player_count]
 
+    for idx, val in enumerate(strat_count) :
+        dim.append(int(val))
+        transpose_axes.append(idx)
+        element_count *= int(val)
+
+    dim.reverse()
+    transpose_axes.reverse()
+
+    elements = input().strip().split(' ')
+    assert len(elements) == element_count, " Incorrect number of Utilities Provided"
+    
+    strat_matrix = np.empty(element_count)
+    
+    for idx, val in enumerate(elements) :
+        strat_matrix[idx] = float(val)
+    
+    reshaped = strat_matrix.reshape(tuple(dim))
+    return reshaped.transpose(transpose_axes), player_count, strat_count
 
 def main() :
     
-    assert len(sys.argv) == 2, "Must have exactly 1 File Path"
-    matrix = get_data_from(sys.argv[1])
-    print(matrix)
+    utility_matrix, player_count, strat_count = get_input()
+    print(utility_matrix)
 
 if __name__ == '__main__':
     main()
